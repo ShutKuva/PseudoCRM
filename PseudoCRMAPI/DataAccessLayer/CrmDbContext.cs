efbfeff62cb30516af4017a0600cbb6bf1fa8fc1
Shut Kuva <zekazyza763@gmail.com>
2023-03-22 14:25:51 +0200

    Initial

---
+++ PseudoCRMAPI/DataAccessLayer/CrmDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class CrmDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+    }
+}
