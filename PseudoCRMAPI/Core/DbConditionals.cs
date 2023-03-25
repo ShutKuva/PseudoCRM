87c860a80af6d2bdc45876407dca589592fbf102
Shut Kuva <zekazyza763@gmail.com>
2023-03-25 18:41:48 +0200

    Adding abstractions

---
+++ PseudoCRMAPI/Core/DbConditionals.cs
@@ -0,0 +1,11 @@
+﻿namespace Core
+{
+    public abstract class DbCredentials
+    {
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public string? ConnectionString { get; set; }
+
+        public abstract IEnumerable<string> GetAllTables();
+    }
+}
--- PseudoCRMAPI/Core/User.cs
+++ PseudoCRMAPI/Core/User.cs
@@ -7,5 +7,6 @@ namespace Core
         public string Name { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
+        
     }
 }
