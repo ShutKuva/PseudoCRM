f16fe8fb47559acd64c004d12b85876429dad023
Shut Kuva <zekazyza763@gmail.com>
2023-03-26 22:35:44 +0300

    Added more generics for interfaces

--- PseudoCRMAPI/BusinessLogicLayer.Abstractions/Email/IEmailService.cs
+++ PseudoCRMAPI/BusinessLogicLayer.Abstractions/Email/IEmailService.cs
@@ -4,16 +4,16 @@ using MailKit.Search;
 
 namespace BusinessLogicLayer.Abstractions.Email
 {
-    public interface IEmailService<U, M>
+    public interface IEmailService<U, M, Q, E>
     {
         Task<IReadOnlyList<M>> GetMessages(U user, string publicName, ServerProtocols serverProtocol = ServerProtocols.Imap);
 
         Task<IReadOnlyList<M>> GetMessages(U user, string publicName, int takeLast);
 
-        Task<IReadOnlyList<M>> GetMessages(U user, string publicName, SearchQuery searchQuery);
+        Task<IReadOnlyList<M>> GetMessages(U user, string publicName, Q searchQuery);
 
         Task SendMessage(U user, string publicName, M message);
 
-        Task SetNewEmail(U user, EmailCredentials emailCredentials);
+        Task SetNewEmail(U user, E emailCredentials);
     }
 }
--- PseudoCRMAPI/BusinessLogicLayer/Email/Adapters/TextMessageSender.cs
+++ PseudoCRMAPI/BusinessLogicLayer/Email/Adapters/TextMessageSender.cs
@@ -1,4 +1,5 @@
-﻿using BusinessLogicLayer.Abstractions.Email.Adapters;
+﻿using BusinessLogicLayer.Abstractions.Email;
+using BusinessLogicLayer.Abstractions.Email.Adapters;
 using Core;
 using Core.Email;
 
@@ -6,5 +7,14 @@ namespace BusinessLogicLayer.Email.Adapters
 {
     public class TextMessageSender : IMessageSender<User, EmailTextMessage>
     {
+        public TextMessageSender(IEmailService<User, MimeKit>)
+        {
+            
+        }
+
+        public void SendMessage(User user, EmailTextMessage message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
