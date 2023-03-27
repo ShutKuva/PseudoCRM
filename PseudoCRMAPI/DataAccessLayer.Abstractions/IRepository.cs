e6e0ffb7e1cceba7f014736131fd135461c29f2f
Shut Kuva <zekazyza763@gmail.com>
2023-03-27 10:10:16 +0300

    Fixed repository methods

--- PseudoCRMAPI/DataAccessLayer.Abstractions/IRepository.cs
+++ PseudoCRMAPI/DataAccessLayer.Abstractions/IRepository.cs
@@ -7,13 +7,11 @@ namespace DataAccessLayer.Abstractions
     {
         void Create(T entity);
         Task CreateAsync(T entity);
-        T? Read(U id);
         Task<T?> ReadAsync(U id);
         IEnumerable<T> ReadByCondition(Expression<Func<T, bool>> predicate, int skip, int take);
         Task<IEnumerable<T>> ReadByConditionAsync(Expression<Func<T, bool>> predicate, int skip, int take);
-        void Update(T entity);
         Task UpdateAsync(T entity);
-        void Delete(U id);
-        Task DeleteAsync(U id);
+        void Delete(T entity);
+        Task DeleteAsync(T entity);
     }
 }
